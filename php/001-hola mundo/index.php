78563e262c95043f451fcfcc7f752f9f84cab4cc
alisgoes77 <110934809+alisgoes77@users.noreply.github.com>
2022-08-29 23:21:12 -0500

    Add files via upload

---
+++ php/001-hola mundo/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hola Mundo en PHP</title>
+</head>
+<body>
+	<H1>Hola mundo, en <strong>PHP</strong></H1>
+	<p>
+			<em>
+			<?php
+			echo "<h2>hola mundo</h2>";
+			?>
+			</em>
+	</p>
+</body>
+</html>
